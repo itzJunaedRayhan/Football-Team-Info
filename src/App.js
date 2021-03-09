c9660c781529ff321401d2827907599228f20250
Junaed Rayhan <junaedrayhan@gmail.com>
2021-03-10 01:38:18 +0600

    Environment Ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Allah Mohan</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="*">
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+const NotFound = () => {
+    return (
+        <div style={{color: 'red'}} className="text-center my-5">
+            <h2>Sorry, Module Not Found</h2>
+            <h4>Error 404 !</h4>
+            <Link to={`/home`}>
+                <button className="btn btn-warning my-5 px-5 text-white"><FontAwesomeIcon icon={faArrowLeft} /> Go Home</button>
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
